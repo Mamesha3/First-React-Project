91fcf1dfdbf7e5e37ba3908bbf4ed6d1df297c12
Mohammed Ahmed <webmame08@gmail.com>
2026-02-02 23:53:26 +0300

    update deploy part

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    hmr: {
-      overlay: false
-    }
-  }
+  base: "/First-React-Project/"
 })
